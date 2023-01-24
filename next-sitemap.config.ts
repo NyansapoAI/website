0019d07f47d9adaeee1f5dfc0e635409b586730a
alexmagwe <alexmagwe@gmail.com>
2023-01-24 20:28:54 +0300

    fix:added import to sitemap-config

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
+const config: import('next-sitemap').IConfig = {
   siteUrl: process.env.SITE_URL || 'https://nyansapoai.net',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
+export default config
