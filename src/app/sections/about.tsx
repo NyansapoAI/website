d1fed7508d5a79e303e9aede12e82d34d8cbf2d2
alexmagwe <alexmagwe@gmail.com>
2023-01-26 13:11:26 +0300

    feat:added why us cards

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import SectionTitle from '../components/SectionHeader'
-
+import time from '@imgs/about/time.png'
+import digital from '@imgs/about/digital.png'
+import track from '@imgs/about/track.png'
+import Image from 'next/image'
 type Props = {}
 
 export default function About({}: Props) {
@@ -10,6 +13,36 @@ export default function About({}: Props) {
         Nyansapo AI improves literacy and numeracy in marginalized communities
         through AI-based assessments, grouping, and activities.
       </SectionTitle>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-8'>
+        <Card imageUrl={time.src}>Reduce Assessment time and hustle</Card>
+        <Card imageUrl={digital.src}>
+          No more manual data collection on paper
+        </Card>
+        <Card imageUrl={track.src}>
+          Track individual learning levels over time
+        </Card>
+      </div>
     </section>
   )
 }
+
+type CardProps = {
+  imageUrl: string
+  children: ReactNode | string
+}
+
+function Card({ imageUrl, children }: CardProps) {
+  return (
+    <div className='flex flex-col gap-6'>
+      <Image
+        width={300}
+        className='object-contain aspect-square'
+        height={300}
+        src={imageUrl}
+        alt='illustration'
+      />
+      <p className='text-xl font-semibold leading-tight'>{children}</p>
+    </div>
+  )
+}
+;``
