e88295a2dfd873a913255be6f2a375fd07518bad
alexmagwe <alexmagwe@gmail.com>
2023-01-26 13:11:47 +0300

    fix:improved responsiveness on xs screens

--- src/app/components/HeroGallery.tsx
+++ src/app/components/HeroGallery.tsx
@@ -6,8 +6,8 @@ type Props = {}
 function HeroGallery({}: Props) {
   return (
     <div className='relative '>
-      <div className='h-64 w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600'></div>
-      <div className='h-80 w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0'></div>
+      <div className='w-48 h-48 sm:h-64 sm:w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600'></div>
+      <div className='w-56 h-56 sm:h-80 sm:w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0'></div>
       <div className='z-50 hover:scale-125 duration-300 relative'>
         <Image
           src={image1.src}
@@ -16,46 +16,6 @@ function HeroGallery({}: Props) {
           alt='children doing activities'
         />
       </div>
-      {/* <div className='z-20 left-48 -top-10 absolute'>
-        <Image
-          src={image1.src}
-          width={200}
-          height={200}
-          alt='children doing activities'
-        />
-      </div>
-      <div className='z-10 absolute -right-20 top-20'>
-        <Image
-          src={image1.src}
-          width={250}
-          height={250}
-          alt='children doing activities'
-        />
-      </div>
-      <div className='z-40 -bottom-20 left-20 absolute'>
-        <Image
-          src={image1.src}
-          width={320}
-          height={320}
-          alt='children doing activities'
-        />
-      </div>
-      <div className='z-10 -bottom-40 left-80 absolute'>
-        <Image
-          src={image1.src}
-          width={300}
-          height={300}
-          alt='children doing activities'
-        />
-      </div>
-      <div className='z-10 -bottom-48  right-96 absolute'>
-        <Image
-          src={image1.src}
-          width={300}
-          height={300}
-          alt='children doing activities'
-        />
-      </div> */}
     </div>
   )
 }
