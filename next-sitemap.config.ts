19843b7b73904ec47726fe6e7329c3bd5f5f678d
alexmagwe <alexmagwe@gmail.com>
2023-02-08 22:16:18 +0300

    update:switched sitemap to js

--- next-sitemap.config.ts
+++
@@ -1,6 +0,0 @@
-const config: import('next-sitemap').IConfig = {
-  siteUrl: process.env.SITE_URL || 'https://nyansapoai.net',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
-export default config
