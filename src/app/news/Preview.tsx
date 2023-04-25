af144399ea3a73df71c07fedb0983c1ca53374c0
alexmagwe <alexmagwe@gmail.com>
2023-04-25 13:17:32 +0300

    update:improved responsiveness

--- src/app/news/Preview.tsx
+++ src/app/news/Preview.tsx
@@ -16,7 +16,7 @@ export default function Preview({ data }: Props) {
   return (
     <div className="">
       <Link
-        className="flex flex-col justify-start items-start gap-4"
+        className="group flex flex-col justify-start items-start gap-4"
         href={`/news/${data._id}`}
       >
         {data.mainImage && (
@@ -24,13 +24,13 @@ export default function Preview({ data }: Props) {
             <Image
               alt={data.slug.current}
               {...imageProps}
-              className="w-80  bg-cover aspect-auto hover:scale-125 duration-300" // layout="responsive" prior to Next 13.0.0
+              className="w-80  bg-cover aspect-auto group-hover:scale-125 duration-300" // layout="responsive" prior to Next 13.0.0
               placeholder="blur"
               blurDataURL={data.mainImage.asset.metadata.lqip}
             />
           </div>
         )}
-        <h1 className="text-2xl w-80 text-center lg:text-left hover:text-slate-600 duration-300">
+        <h1 className="text-2xl w-80 text-center lg:text-left group-hover:text-slate-500 duration-300">
           {data.title}
         </h1>
       </Link>
