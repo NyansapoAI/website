9ee9c52a6763a7b062b86f7622c982bb455915ac
alexmagwe <alexmagwe@gmail.com>
2023-07-31 13:35:12 +0300

    update:change hero image

--- src/app/components/HeroGallery.tsx
+++ src/app/components/HeroGallery.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
-import image1 from '@imgs/hero/0.png'
+import Image from "next/image"
+import React from "react"
+import image1 from "@imgs/hero/1.png"
 type Props = {}
 
 function HeroGallery({}: Props) {
   return (
-    <div className='relative '>
-      <div className='w-48 h-48 sm:h-64 sm:w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600'></div>
-      <div className='w-56 h-56 sm:h-80 sm:w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0'></div>
-      <div className='z-50 hover:scale-125 duration-300 relative'>
+    <div className="relative ">
+      <div className="w-48 h-48 sm:h-64 sm:w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600"></div>
+      <div className="w-56 h-56 sm:h-80 sm:w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0"></div>
+      <div className="z-50  duration-300 relative">
         <Image
           src={image1.src}
           width={800}
           height={800}
-          alt='children doing activities'
+          alt="children doing activities"
         />
       </div>
     </div>
