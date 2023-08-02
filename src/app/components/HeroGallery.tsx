2f517d2542f35a6adb87dfee027327033d53de3e
alexmagwe <alexmagwe@gmail.com>
2023-08-02 09:22:51 +0300

    update:remove bg graphics

--- src/app/components/HeroGallery.tsx
+++ src/app/components/HeroGallery.tsx
@@ -6,8 +6,8 @@ type Props = {}
 function HeroGallery({}: Props) {
   return (
     <div className="relative ">
-      <div className="w-48 h-48 sm:h-64 sm:w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600"></div>
-      <div className="w-56 h-56 sm:h-80 sm:w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0"></div>
+      {/* <div className="w-48 h-48 sm:h-64 sm:w-64 absolute left-10 bottom-0 rounded-full z-0 bg-cyan-600"></div> */}
+      {/* <div className="w-56 h-56 sm:h-80 sm:w-80 absolute top-0 right-10 rounded-full bg-cyan-200 z-0"></div> */}
       <div className="z-50  duration-300 relative">
         <Image
           src={image1.src}
