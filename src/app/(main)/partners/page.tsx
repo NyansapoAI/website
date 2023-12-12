0c5bf19379f29502931129e79d5db944949ed62d
kepla <47972920+alexmagwe@users.noreply.github.com>
2023-12-12 18:39:31 +0300

    Update page.tsx

--- src/app/(main)/partners/page.tsx
+++ src/app/(main)/partners/page.tsx
@@ -24,6 +24,7 @@ export interface IPartner {
     }
   }
 }
+export const revalidate=60*60
 export default async function Achievements() {
   const data = await clientFetch<IPartner[]>(query)
   return (
