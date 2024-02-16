a71dcca53e1fb3963736edbae103a19d345b208c
alexmagwe <alexmagwe@gmail.com>
2024-02-16 12:58:39 +0300

    Remove pricing link from navigationLinks

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,7 +2,7 @@ export const navigationLinks = [
   // { name: "about us", type: "section" },
   { name: "products", type: "section" },
   { name: "about us", link: "about", type: "page" },
-  { name: "pricing", link: "pricing", type: "page" },
+  // { name: "pricing", link: "pricing", type: "page" },
   {
     name: "impact",
     type: "menu",
